c4a87e28d724fb3f5e6f1291e8c8dcba71a13b74
Rubens Saraiva <rubens.saraiva@gmail.com>
2015-09-07 14:49:43 -0300

    Cria o modulo tomcat

--- grupo-estudo-devops/manifests/web.pp
+++ grupo-estudo-devops/manifests/web.pp
@@ -1,59 +1,40 @@
-exec { "apt-update":
-	command => "/usr/bin/apt-get update"
+include mysql::client
+
+$keystore_file = "/etc/ssl/.keystore"
+
+$ssl_connector = {
+	"port" => 8443,
+	"protocol" => "HTTP/1.1",
+	"SSLEnabled" => true,
+	"maxThreads" => 150,
+	"scheme" => "https",
+	"secure" => "true",
+	"keystoreFile" => "$keystore_file",
+	"keystorePass" => "secret",
+	"clientAuth" => false,
+	"sslProtocol" => "SSLv3",
 }
 
-package { ["mysql-client", "tomcat7"]:
-	ensure => installed,
-	require => Exec["apt-update"],
+$db = {
+	"user" => "loja",
+	"password" => "lojasecret",
+	"driver" => "com.mysql.jdbc.Driver",
+	"url" => "jdbc:mysql://192.168.33.10:3306/loja_schema",
 }
 
-file { "/var/lib/tomcat7/conf/.keystore":
-	owner => root,
-	group => tomcat7,
+file { $keystore_file:
 	mode => 0640,
 	source => "/vagrant/manifests/.keystore",
-	require => Package["tomcat7"],
-	notify => Service["tomcat7"],
 }
 
-file { "/var/lib/tomcat7/conf/server.xml":
-	owner => root,
-	group => tomcat7,
-	mode => 0644,
-	source => "/vagrant/manifests/server.xml",
-	require => Package["tomcat7"],
-	notify => Service["tomcat7"],
-}
-
-file { "/etc/default/tomcat7":
-	owner => root,
-	group => root,
-	mode => 0644,
-	source => "/vagrant/manifests/tomcat7",
-	require => Package["tomcat7"],
-	notify => Service["tomcat7"],
-}
-
-service { "tomcat7":
-	ensure => running,
-	enable => true,
-	hasstatus => true,
-	hasrestart => true,
-	require => Package["tomcat7"],
-}
-
-$db_host = "192.168.33.10"
-$db_schema = "loja_schema"
-$db_user = "loja"
-$db_password = "lojasecret"
-
-file { "/var/lib/tomcat7/conf/context.xml":
-	owner => root,
-	group => tomcat7,
-	mode => 0644,
-	content => template("/vagrant/manifests/context.xml"),
-	require => Package["tomcat7"],
-	notify => Service["tomcat7"],
+class { "tomcat::server":
+	connectors => [$ssl_connector],
+	data_sources => {
+		"jdbc/web" => $db,
+		"jdbc/secure" => $db,
+		"jdbc/storage" => $db,
+	},
+	require => File[$keystore_file],
 }
 
 file { "/var/lib/tomcat7/webapps/devopsnapratica.war":
