7b4287f793b64a58dd09db5640e62b0d9c665f52
Rubens Saraiva <rubens.saraiva@gmail.com>
2015-09-10 22:09:40 -0300

    Configurando o jenkins pelo puppet.

--- grupo-estudo-devops/modules/loja_virtual/manifests/ci.pp
+++ grupo-estudo-devops/modules/loja_virtual/manifests/ci.pp
@@ -29,4 +29,39 @@ class loja_virtual::ci {
 	]
 
 	jenkins::plugin { $plugins: }
+
+	file { '/var/lib/jenkins/hudson.tasks.Maven.xml':
+		mode => 0644,
+		owner => 'jenkins',
+		group => 'jenkins',
+		source => 'puppet:///modules/loja_virtual/hudson.tasks.Maven.xml',
+		require => Class['jenkins::package'],
+		notify => Service['jenkins'],
+	}
+
+	$job_structure = [
+		'/var/lib/jenkins/jobs/',
+		'/var/lib/jenkins/jobs/loja-virtual-devops'
+	]
+
+	$git_repository = 'https://github.com/rsaraiva/loja-virtual-devops.git'
+	$git_poll_interval = '* * * * *'
+	$maven_goal = 'install'
+	$archive_artifacts = 'combined/target/*.war'
+
+	file { $job_structure:
+		ensure => 'directory',
+		owner => 'jenkins',
+		group => 'jenkins',
+		require => Class['jenkins::package'],
+	}
+
+	file { "${job_structure[1]}/config.xml":
+		mode => 0644,
+		owner => 'jenkins',
+		group => 'jenkins',
+		content => template('loja_virtual/config.xml'),
+		require => File[$job_structure],
+		notify => Service['jenkins'],
+	}
 }
